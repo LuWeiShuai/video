8e4767dcbb53ff5403c4d00e510c621cb4a12d2d
helloworld <772379768@qq.com>
2017-11-20 14:07:15 +0800

    han

--- project/app/Http/Kernel.php
+++ project/app/Http/Kernel.php
@@ -30,6 +30,5 @@ class Kernel extends HttpKernel
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'admin_login' => \App\Http\Middleware\aloginMiddleware::class
-        'home_login' => \App\Http\Middleware\hloginMiddleware::class
     ];
 }
