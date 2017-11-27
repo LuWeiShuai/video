c4c4d52e16ed508b86aa8fe376f9d1dcd92f08a1
helloworld <772379768@qq.com>
2017-11-27 09:39:50 +0800

    han

--- project/resources/views/home/video/list.blade.php
+++ project/resources/views/home/video/list.blade.php
@@ -11,7 +11,7 @@
 
 						<div class="col-md-3 resent-grid recommended-grid">
 							<div class="resent-grid-img recommended-grid-img">
-								<a href="{{ url('/home/play/'.$v->id)}}"><img src="/admins/video/upload/{{$v->logo}}" alt="" style="height:200px;"></a>
+								<a href="{{ url('/home/play/'.$v->id)}}"><img src="http://ozssihjsk.bkt.clouddn.com/images/{{$v->logo}}" alt="" style="height:200px;"></a>
 								<div class="time small-time">
 									<p><i style="font-size: 18px;color: red;">{{$v->level}}分</i></p>	
 								</div>
