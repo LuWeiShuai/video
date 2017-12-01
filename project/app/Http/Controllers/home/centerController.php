7a53a46e611bfd68fa0919146481e7c3dbe55db8
LuWeiShuai <853870163@qq.com>
2017-12-01 15:47:27 +0800

    tj

--- project/app/Http/Controllers/home/centerController.php
+++ project/app/Http/Controllers/home/centerController.php
@@ -19,6 +19,8 @@ use App\Http\model\config;
 use App\Http\model\money;
 use App\Http\model\video;
 use App\Http\model\user_vip;
+use App\Http\model\uhistory;
+
 
 class centerController extends Controller
 {
@@ -219,8 +221,10 @@ class centerController extends Controller
 
         //从history数据库查询
         $res = history::where('uid',$uid)->orderBy('time','desc')->get();
+        $res1 = uhistory::where('uid',$uid)->orderBy('time','desc')->get();
 
-        return view('/home/center/history',['res'=>$res]);
+
+        return view('/home/center/history',['res'=>$res,'res1'=>$res1]);
     }
 
     //执行删除历史记录
@@ -228,8 +232,10 @@ class centerController extends Controller
     {
         //删除数据库中的数据
         $res = history::where('id',$id)->delete();
+        $res1 = uhistory::where('id',$id)->delete();
+
         //判断
-        if ($res) {
+        if ($res || $res1) {
             return redirect('/home/center/history')->with('msg','删除成功');
         }else{
             return back()->with('msg','删除失败');
--- project/resources/views/home/center/history.blade.php
+++ project/resources/views/home/center/history.blade.php
@@ -55,6 +55,36 @@
 
 			</div>
 		</div>
+	@foreach($res1 as $k => $v)
+				<div class="col-md-3 resent-grid recommended-grid">
+					<div class="resent-grid-img recommended-grid-img" style="margin-top: 15px;">
+						<a href="{{ url('/home/play/'.$v->vid)}}">
+							<img src="http://ozssihjsk.bkt.clouddn.com/images/{{$v->logo}}" style="height:150px;width: 100%;">
+						</a>
+					</div>
+					
+					<div class="resent-grid-info recommended-grid-info video-info-grid">
+						<ul>
+							<li>
+								<a href="{{ url('/home/play/'.$v->vid)}}" class="title">{{$v->title}}</a>
+
+							</li>
+						</ul>
+						<ul>
+							<li>
+								<p class="author author-info">最后播放时间：{{$v->time}}</p>
+							</li>
+							<li>
+								<form action="{{ url('/home/center/delete/'.$v->id)}}" method="get">
+									<button type="submit" class="btn btn-danger">删除记录</button>
+
+								</form>
+								
+							</li>
+						</ul>
+					</div>
+				</div>
+				@endforeach
 
 	</div>
 @endsection
