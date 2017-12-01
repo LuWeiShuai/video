e0d4db0428e701036f48d1481e0ac11c74efba92
qq175023117 <175023117@qq.com>
2017-12-01 14:53:32 +0800

    wxq

--- project/resources/views/home/center/history.blade.php
+++ project/resources/views/home/center/history.blade.php
@@ -23,36 +23,42 @@
 			<div class="recommended-grids">
 				
 				@foreach($res as $k => $v)
-				<div class="col-md-3 resent-grid recommended-grid">
-					<div class="resent-grid-img recommended-grid-img" style="margin-top: 15px;">
-						<a href="{{ url('/home/play/'.$v->vid)}}">
-							<img src="http://ozssihjsk.bkt.clouddn.com/images/{{$v->logo}}" style="height:150px;width: 100%;">
-						</a>
-					</div>
-					
-					<div class="resent-grid-info recommended-grid-info video-info-grid">
-						<ul>
-							<li>
-								<a href="{{ url('/home/play/'.$v->vid)}}" class="title">{{$v->title}}</a>
+					<?php
+				        //从video表中查询
+				        $video = DB::table('video')->where('id',$v->vid)->first();
+					?>
+					@if($video->status == 1)
+					<div class="col-md-3 resent-grid recommended-grid">
+						<div class="resent-grid-img recommended-grid-img" style="margin-top: 15px;">
+							<a href="{{ url('/home/play/'.$v->vid)}}">
+								<img src="http://ozssihjsk.bkt.clouddn.com/images/{{$v->logo}}" style="height:150px;width: 100%;">
+							</a>
+						</div>
+						
+						<div class="resent-grid-info recommended-grid-info video-info-grid">
+							<ul>
+								<li>
+									<a href="{{ url('/home/play/'.$v->vid)}}" class="title">{{$v->title}}</a>
 
-							</li>
-						</ul>
-						<ul>
-							<li>
-								<p class="author author-info">最后播放时间：{{$v->time}}</p>
-							</li>
-							<li>
-								<form action="{{ url('/home/center/delete/'.$v->id)}}" method="get">
-									<button type="submit" class="btn btn-danger">删除记录</button>
+								</li>
+							</ul>
+							<ul>
+								<li>
+									<p class="author author-info">最后播放时间：{{$v->time}}</p>
+								</li>
+								<li>
+									<form action="{{ url('/home/center/delete/'.$v->id)}}" method="get">
+										<button type="submit" class="btn btn-danger">删除记录</button>
 
-								</form>
-								
-							</li>
-						</ul>
+									</form>
+									
+								</li>
+							</ul>
+						</div>
 					</div>
-				</div>
+					@endif
 				@endforeach
-
+				
 			</div>
 		</div>
 
