3d39fd333a1a06c827a3228d7582271706ef0316
qq175023117 <175023117@qq.com>
2017-12-01 16:32:38 +0800

    wxq

--- project/resources/views/home/center/history.blade.php
+++ project/resources/views/home/center/history.blade.php
@@ -59,38 +59,46 @@
 					@endif
 				@endforeach
 				
+				@foreach($res1 as $key => $val)
+					<?php
+				        //从uvideo表中查询
+				        $uvideo = DB::table('uvideo')->where('id',$val->vid)->first();
+					?>
+					@if($uvideo->status == 1)
+					<div class="col-md-3 resent-grid recommended-grid">
+						<div class="resent-grid-img recommended-grid-img" style="margin-top: 15px;">
+							<a href="{{ url('/home/play/'.$val->vid)}}">
+								<img src="http://ozssihjsk.bkt.clouddn.com/images/{{$val->logo}}" style="height:150px;width: 100%;">
+							</a>
+						</div>
+						
+						<div class="resent-grid-info recommended-grid-info video-info-grid">
+							<ul>
+								<li>
+									<a href="{{ url('/home/play/'.$val->vid)}}" class="title">{{$val->title}}</a>
+
+								</li>
+							</ul>
+							<ul>
+								<li>
+									<p class="author author-info">最后播放时间：{{$val->time}}</p>
+								</li>
+								<li>
+									<form action="{{ url('/home/center/delete/'.$val->id)}}" method="get">
+										<button type="submit" class="btn btn-danger">删除记录</button>
+
+									</form>
+									
+								</li>
+							</ul>
+						</div>
+					</div>
+					@endif
+				@endforeach
 			</div>
 		</div>
-	@foreach($res1 as $k => $v)
-				<div class="col-md-3 resent-grid recommended-grid">
-					<div class="resent-grid-img recommended-grid-img" style="margin-top: 15px;">
-						<a href="{{ url('/home/play/'.$v->vid)}}">
-							<img src="http://ozssihjsk.bkt.clouddn.com/images/{{$v->logo}}" style="height:150px;width: 100%;">
-						</a>
-					</div>
-					
-					<div class="resent-grid-info recommended-grid-info video-info-grid">
-						<ul>
-							<li>
-								<a href="{{ url('/home/play/'.$v->vid)}}" class="title">{{$v->title}}</a>
-
-							</li>
-						</ul>
-						<ul>
-							<li>
-								<p class="author author-info">最后播放时间：{{$v->time}}</p>
-							</li>
-							<li>
-								<form action="{{ url('/home/center/delete/'.$v->id)}}" method="get">
-									<button type="submit" class="btn btn-danger">删除记录</button>
-
-								</form>
-								
-							</li>
-						</ul>
-					</div>
-				</div>
-				@endforeach
 
 	</div>
+
+
 @endsection
